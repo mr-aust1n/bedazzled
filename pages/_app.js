823df677b1719bcd0d2be4e7fce81790bc00ba4f
MrAust1n <craig@mraustin.co.uk>
2025-02-09 22:15:43 +0000

    schema update

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ const App = ({ Component, pageProps }) => {
     }, 1000);
   }, []);
 
-  // Updated Schema Markup (Structured Data) without pricing
+  // Updated Schema Markup (Structured Data) for Commercial Window Cleaning Only
   const structuredData = {
     "@context": "https://schema.org",
     "@graph": [
@@ -87,7 +87,7 @@ const App = ({ Component, pageProps }) => {
         ],
       },
       {
-        "@type": "LocalBusiness",
+        "@type": "HomeAndConstructionBusiness",
         "@id": "https://bedazzledwcs.co.uk/#localbusiness",
         name: "Bedazzled Window Cleaning Services",
         url: "https://bedazzledwcs.co.uk",
@@ -110,7 +110,6 @@ const App = ({ Component, pageProps }) => {
             closes: "18:00",
           },
         ],
-        serviceType: "Window Cleaning",
         areaServed: [
           "Lancashire",
           "Blackpool",
@@ -135,12 +134,6 @@ const App = ({ Component, pageProps }) => {
             availability: "https://schema.org/InStock",
             url: "https://bedazzledwcs.co.uk/services/commercial-window-cleaning",
           },
-          {
-            "@type": "Offer",
-            name: "Residential Window Cleaning",
-            availability: "https://schema.org/InStock",
-            url: "https://bedazzledwcs.co.uk/services/residential-window-cleaning",
-          },
         ],
         sameAs: [
           "https://www.facebook.com/bedazzledwcs",
@@ -159,7 +152,7 @@ const App = ({ Component, pageProps }) => {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="description"
-          content="North West window cleaning by beDAZzled"
+          content="North West commercial window cleaning by beDAZzled"
         />
         <meta
           name="viewport"
@@ -167,46 +160,6 @@ const App = ({ Component, pageProps }) => {
         />
         {/*====== Title ======*/}
         <title>BeDAZzled Commercial Window Cleaning - North West - UK</title>
-        {/*====== Favicon Icon ======*/}
-        <link
-          rel="shortcut icon"
-          href="assets/images/favicon.ico"
-          type="image/png"
-        />
-        {/*====== Google Fonts ======*/}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-        {/*====== Stylesheets ======*/}
-        <link
-          rel="stylesheet"
-          href="assets/fonts/flaticon/flaticon_gadden.css"
-        />
-        <link
-          rel="stylesheet"
-          href="assets/fonts/fontawesome/css/all.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="assets/vendor/bootstrap/css/bootstrap.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="assets/vendor/magnific-popup/dist/magnific-popup.css"
-        />
-        <link rel="stylesheet" href="assets/vendor/slick/slick.css" />
-        <link
-          rel="stylesheet"
-          href="assets/vendor/jquery-ui/jquery-ui.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="assets/vendor/nice-select/css/nice-select.css"
-        />
-        <link rel="stylesheet" href="assets/vendor/animate.css" />
-        <link rel="stylesheet" href="assets/css/default.css" />
-        <link rel="stylesheet" href="assets/css/style.css" />
         {/*====== JSON-LD Schema Markup ======*/}
         <script
           type="application/ld+json"
