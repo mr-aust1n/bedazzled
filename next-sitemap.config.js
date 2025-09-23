9a7c031d826928151dc9ef9524249eee87d9d9aa
MrAust1n <craig@mraustin.co.uk>
2025-09-23 21:50:07 +0100

    Sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.bedazzledwcs.co.uk", // replace with your real domain
+  generateRobotsTxt: true, // also generates robots.txt
+  sitemapSize: 5000,
+};
